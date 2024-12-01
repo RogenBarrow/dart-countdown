f3fc0b2a9c6726a1d3c330eb34ff01a8941b2d4e
Devratb <93407364+Devratb@users.noreply.github.com>
2024-12-01 11:20:49 -0400

    project creation

---
+++ bin/dart_countdown.dart
@@ -0,0 +1,5 @@
+import 'package:dart_countdown/dart_countdown.dart' as dart_countdown;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_countdown.calculate()}!');
+}
---
+++ test/dart_countdown_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_countdown/dart_countdown.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
